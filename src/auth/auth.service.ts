d8937ae8ceeba38c6cd4f300ab847470b916799a
Kate Bastow <kate.b@montu.com.au>
2025-11-14 17:36:34 +1100

    add username and password validation options, add bcrypt to hash and salt passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDTO } from './dto/auth-credentials.dta';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,7 +19,21 @@ export class AuthService {
   async createUser(authCredentialsDto: AuthCredentialsDTO): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.usersRepository.create({ username, password });
-    await this.usersRepository.save(user);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.usersRepository.create({
+      username,
+      password: hashedPassword,
+    });
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code === 23505) {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException('Failed to create new user');
+      }
+    }
   }
 }
